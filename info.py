895a58227dc613d2f1283bece1635f3d83d88d1c
benzmawa <benzmawa289@gmail.com>
2024-12-15 22:19:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://benz:dFcz1seFRokZ52DC@cluster0.q70dv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://pawandeep12284:zDsaDVHh0How89hm@cluster0.ndd7kxh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "benz")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'benzmawafiles"')
 
